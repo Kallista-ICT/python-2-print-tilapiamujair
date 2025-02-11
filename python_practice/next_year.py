d9bd49c877354a6e07a693b091b840ebf24162c5
tilapiamujair <mochacinot@gmail.com>
2025-02-11 07:18:52 +0000

    submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,8 @@
+print("hello,i'm kiryu (˶ˆᗜˆ˵) ")
+name = input ("what's your name ?")
+print(name + " ,that's a cute name (˶ᵔ ᵕ ᵔ˶) ")
+age = int(input("i am 16 years old, you? "))
+print(f"so, {name} you are {age} now")
+print(f"so, next year you will be {age + 1} years old")
+print("happy early birthday " +  name + "(˶˃ ᵕ ˂˶)")
+print("yay you finish your work ≽^•⩊•^≼")
